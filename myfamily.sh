bec5688b75802a852835063608ef3bde7f92dde6
alseiitov <dossan.alseiit@gmail.com>
2019-10-14 19:49:39 +0600

    fam

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 export HERO_ID=0
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json|jq -r '.[0].connections.relatives'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json|jq -r '.[52].connections.relatives'
