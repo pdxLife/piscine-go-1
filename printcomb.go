8c0c5e47f68dbbf914b4079136ac1be3ec7e98ab
alseiitov <dossan.alseiit@gmail.com>
2019-10-16 16:43:37 +0600

    m

--- printcomb.go
+++ printcomb.go
@@ -7,9 +7,9 @@ func PrintComb() {
 		for b := 48; b <= 57; b++ {
 			for c := 48; c <= 57; c++ {
 				if a < b && b < c {
-					z01.PrintRune(rune(a))
-					z01.PrintRune(rune(b))
-					z01.PrintRune(rune(c))
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
 					if a != 55 {
 						z01.PrintRune(44)
 						z01.PrintRune(32)
