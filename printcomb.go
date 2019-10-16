3d676b0fb555011399436e67851ad3a609a55a8d
alseiitov <dossan.alseiit@gmail.com>
2019-10-16 17:03:04 +0600

    m

--- printcomb.go
+++ printcomb.go
@@ -3,16 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := 48; a <= 57; a++ {
-		for b := 48; b <= 57; b++ {
-			for c := 48; c <= 57; c++ {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
 				if a < b && b < c {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(c)
-					if a != 55 {
-						z01.PrintRune(44)
-						z01.PrintRune(32)
+					if a < 55 {
+					z01.PrintRune(44)
+					z01.PrintRune(32)
 					}
 				}
 			}
