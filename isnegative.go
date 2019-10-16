513117dbfba2344f38065ef2ec80fc9d71ea1b12
alseiitov <dossan.alseiit@gmail.com>
2019-10-16 17:22:06 +0600

    m

--- isnegative.go
+++ isnegative.go
@@ -1,4 +1,4 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
@@ -6,6 +6,6 @@ func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
 	} else {
- 		z01.PrintRune('F')
+		z01.PrintRune('F')
 	}
 }
