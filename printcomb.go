3304c835234d692ca0308db9d4d51130369010e9
alseiitov <dossan.alseiit@gmail.com>
2019-10-16 16:40:27 +0600

    m

---
+++ printcomb.go
@@ -0,0 +1,20 @@
+package piscine
+import "github.com/01-edu/z01"
+func PrintComb() {
+	for a := 48; a <= 57; a++ {
+		for b := 48; b <= 57; b++ {
+			for c := 48; c <= 57; c++ {
+				if a < b && b < c {
+					z01.PrintRune(rune(a))
+					z01.PrintRune(rune(b))
+					z01.PrintRune(rune(c))
+					if a != 55 {
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune(10)
+}
