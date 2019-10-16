449a0d75c859fb8b6304e60aaaeae30831ee7006
alseiitov <dossan.alseiit@gmail.com>
2019-10-16 09:38:48 +0600

    alp

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,11 +1,11 @@
-package  main
+package main
 import (
 	"fmt"
 	"github.com/01-edu/z01"
 )
 func main() {
-		for i := 97; i <= 122; i++ {
+	for i := 97; i <= 122; i++ {
 		z01.PrintRune(rune(i))
-			}
+				}
 	fmt.Print("\n");
 }
