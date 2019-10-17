df4fe25b263445fdb9e2ec0e8e06d624cfe8cdbf
alseiitov <dossan.alseiit@gmail.com>
2019-10-17 18:51:33 +0600

    add

--- strrev.go
+++ strrev.go
@@ -2,8 +2,13 @@ package piscine
 
 func StrRev(s string) string {
 	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
+	nc := 0
+	nword := ""
+	for range s {
+		nc = nc + 1
 	}
-	return string(r)
+	for i := nc - 1; i >= 0; i-- {
+		nword = nword + string(r[i])
+	}
+	return string(nword)
 }
