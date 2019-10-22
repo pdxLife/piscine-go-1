b8609d191e4fe34fe46a7084e46aa695664f61b1
alseiitov <dossan.alseiit@gmail.com>
2019-10-22 11:09:42 +0600

    D

--- alphacount.go
+++ alphacount.go
@@ -2,8 +2,12 @@ package piscine
 
 func AlphaCount(str string) int {
 	counter := 0
-	for i := 0; i < len(str); i++ {
-		if str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z'{
+	a := 0
+	for range str {
+		a++
+	}
+	for i := 0; i < a; i++ {
+		if str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z' {
 			counter++
 		}
 	}
