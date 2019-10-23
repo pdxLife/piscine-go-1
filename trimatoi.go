1530bad0c36130396a2dc5f13d8911a0123cf595
alseiitov <dossan.alseiit@gmail.com>
2019-10-23 09:26:42 +0600

    trimatoi

--- trimatoi.go
+++ trimatoi.go
@@ -1,4 +1,36 @@
-package piscine
+package student
+
+func TrimAtoi(s string) int {
+	if s == "" {
+		return 0
+	}
+	x := 0
+	k := 1
+	s1 := AlphaCount(s)
+
+	if s != "" {
+		for i, n := range s1 {
+			y := 0
+			if n < '0' || n > '9' {
+				if n == '-' || n == '+' {
+					if i != 0 {
+						return 0
+					}
+					if n == '-' {
+						k = -1
+					}
+				} else {
+					return 0
+				}
+			}
+			for i := '1'; i <= n; i++ {
+				y++
+			}
+			x = x*10 + y
+		}
+	}
+	return x * k
+}
 
 func AlphaCount(str string) []rune {
 	//result := 0
