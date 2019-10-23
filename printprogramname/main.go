5f73c4b9dfdc995091347c8fcd1d944a898e73fc
alseiitov <dossan.alseiit@gmail.com>
2019-10-23 16:29:14 +0600

    sddsds

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
- 	"os"
- 	"github.com/01-edu/z01"
- )
- 
- func main() {
- 	arg := os.Args[0]
- 	for _, a := range arg {
- 		z01.PrintRune(a)
- 	}
- 	z01.PrintRune(10)
- }
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arg := os.Args[0]
+	for _, a := range arg {
+		z01.PrintRune(a)
+	}
+	z01.PrintRune(10)
+}
